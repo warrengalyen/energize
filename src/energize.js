6ce19822cf68082a020645ebe36cfbd74c580067
wgalyen <wgalyen@bellhelmets.com>
2019-09-25 13:33:07 -0700

    Bug fix for no cache

--- src/energize.js
+++ src/energize.js
@@ -177,10 +177,10 @@ function _onItemLoad (item, itemList, isAlreadyLoaded) {
     this.totalWeight = 0
     this.loadingSignal = new ShortSignal()
     this._onLoading(item, itemList, loadingSignal, 1, 1)
-    if (item.noCache) _removeItemCache()
+    if (item.noCache) _removeItemCache(item)
   } else {
     this._onLoading(item, itemList, loadingSignal, 1, this.loadedWeight / this.totalWeight)
-    if (item.noCache) _removeItemCache()
+    if (item.noCache) _removeItemCache(item)
     if (!isAlreadyLoaded) {
       this.loadNext()
     }
